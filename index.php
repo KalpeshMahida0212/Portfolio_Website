8da60b8fc97a4523a142cb4a39b8d6c90d6920a2
Predator <mahidak96@gmail.com>
2023-02-04 19:51:38 +0530

    Commit
    Commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalpesh Mahida</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <?php
+include("pointer.php"); ?>
+    <nav class="nav">
+        <div class="container">
+            <div class="logo">
+                <a href="#">Kalpesh Mahida </a>
+            </div>
+            <div id="mainListDiv" class="main_list">
+                <ul class="navlinks">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <span class="navTrigger">
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+        </div>
+    </nav> 
+    <div class="centered-div">
+<?php
+include("animation.php");
+?> 
+   
+  </div>
+
+<!-- partial -->
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r125/three.min.js'></script>
+<script src='https://unpkg.co/gsap@3/dist/gsap.min.js'></script>
+<script src='https://assets.codepen.io/16327/MorphSVGPlugin3.min.js'></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/jsbootstrap.min.js"></script>
+<script src='https://assets.codepen.io/16327/DrawSVGPlugin3.min.js'></script>
+<script src="assets/js/pointer.js"></script>
+<script src="assets/js/script.js"></script>
+
+
+</body>
+</html>
